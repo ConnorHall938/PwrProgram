7250153769efcdf81a3938e503c08504adf8af9d
ConnorHall938 <consparkzz@gmail.com>
2025-02-22 11:14:13 +0000

    Added some more dummy endpoints

--- src/server.js
+++ src/server.js
@@ -8,10 +8,28 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running on port ${port}`);
+});
+
+// Internal for me
+app.get('/api/users', (req,res) => {
+	res.json({message: `Getting users`});
 });
 
 
-app.get('/api/users', (req,res) => {
-	res.json({message: `Hello, world!`});
+// External for the client
+app.get(`/api/clients`, (req,res) => {
+  res.json({message: `Getting current user's clients`});
+});
+
+app.get(`/api/coaches`, (req,res) => {
+  res.json({message: `Getting current user's coaches`});
+});
+
+app.get(`/api/programs`, (req,res) => {
+  res.json({message: `Getting programs for current user`});
+});
+
+app.get(`/api/clients/:id/programs`, (req,res) => {  
+  res.json({message: `Getting programs for client ${req.params.id}`});
 });
